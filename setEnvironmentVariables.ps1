23bbc692bfccc94639a6c6951384990448da0b35
nadvolod <nadvolod@gmail.com>
2019-12-12 14:31:16 -0500

    add rdc api keys

--- setEnvironmentVariables.ps1
+++ setEnvironmentVariables.ps1
@@ -2,15 +2,15 @@
 Param(
 [string]$sauceUserName,
 [string]$sauceAccessKey,
-[string]$sauceHeadlessUserName,
-[string]$sauceHeadlessAccessKey
+[string]$rdcVodQaNativeAppApiKey,
+[string]$rdcSauceDemoIosRdcApiKey
 )
 Write-Output "sauce.userName that was passed in from Azure DevOps=>$sauceUserName"
 Write-Output "sauce.accessKey that was passed in from Azure DevOps=>$sauceAccessKey"
-Write-Output "sauce.headless.userName that was passed in from Azure DevOps=>$sauceHeadlessUserName"
-Write-Output "sauce.headless.access.key that was passed in from Azure DevOps=>$sauceHeadlessAccessKey"
+Write-Output "sauce.rdc.VodQaNativeAppApiKey stored in Azure DevOps=>$rdcVodQaNativeAppApiKey"
+Write-Output "sauce.rdc.SauceDemoIosRdcApiKey stored in Azure DevOps=>$rdcSauceDemoIosRdcApiKey"
 
 [Environment]::SetEnvironmentVariable("SAUCE_USERNAME", "$sauceUserName", "User")
 [Environment]::SetEnvironmentVariable("SAUCE_ACCESS_KEY", "$sauceAccessKey", "User")
-[Environment]::SetEnvironmentVariable("SAUCE_HEADLESS_USERNAME", "$sauceUserName", "User")
-[Environment]::SetEnvironmentVariable("SAUCE_HEADLESS_ACCESS_KEY", "$sauceAccessKey", "User")
+[Environment]::SetEnvironmentVariable("VODQC_RDC_API_KEY", "$rdcVodQaNativeAppApiKey", "User")
+[Environment]::SetEnvironmentVariable("SAUCE_DEMO_IOS_RDC_API_KEY", "$rdcSauceDemoIosRdcApiKey", "User")
