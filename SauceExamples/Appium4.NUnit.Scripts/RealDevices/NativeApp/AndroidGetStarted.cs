930cc948100405421fe6587e9e5a399b02ded6e7
nadvolod <nadvolod@gmail.com>
2020-01-07 17:17:13 -0500

    finished working test

--- SauceExamples/Appium4.NUnit.Scripts/RealDevices/NativeApp/AndroidGetStarted.cs
+++ SauceExamples/Appium4.NUnit.Scripts/RealDevices/NativeApp/AndroidGetStarted.cs
@@ -12,28 +12,32 @@ namespace Appium4.NUnit.Scripts.RealDevices.NativeApp
     public class AndroidGetStarted
     {
         private static string RdcUsHubUrl => "https://us1.appium.testobject.com/wd/hub";
-        private AndroidDriver<IWebElement> _driver;
-        private static readonly string VodQANativeAppApiKey =
-            Environment.GetEnvironmentVariable("VODQC_RDC_API_KEY", EnvironmentVariableTarget.User);
+        private IWebDriver _driver;
 
         [TestMethod]
-        public void TestMethod1()
+        public void ShouldOpenApp()
         {
             var capabilities = new DesiredCapabilities();
             //We can run on any version of the platform as long as it's the correct device
             //Make sure to pick an Android or iOS device based on your app
             capabilities.SetCapability("deviceName", "Google Pixel");
+            capabilities.SetCapability("platformName", "Android");
+
             /*
-             *TODO first you must upload an app to Test Object so that you get your app key
+             * !!!!!!
+             * TODO first you must upload an app to Test Object so that you get your app key
              * Then, make sure you can hardcode it here just to get started
              */
             capabilities.SetCapability("testobject_api_key", new ApiKeys().Rdc.Apps.SampleAppAndroid);
             capabilities.SetCapability("name", MethodBase.GetCurrentMethod().Name);
             capabilities.SetCapability("newCommandTimeout", 90);
 
-            _driver = new AndroidDriver<IWebElement>(new Uri(RdcUsHubUrl), capabilities,
+            _driver = new RemoteWebDriver(new Uri(RdcUsHubUrl), capabilities,
                 TimeSpan.FromSeconds(300));
-            Assert.IsTrue(true);
+            var size = _driver.Manage().Window.Size;
+            Assert.AreNotEqual(0, size.Height);
+            //Always making sure to end the session at the end of any test
+            _driver?.Quit();
         }
     }
 }
