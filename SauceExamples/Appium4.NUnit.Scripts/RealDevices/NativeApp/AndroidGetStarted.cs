ca0e113b2e93408e6500621b2fdea9239b923105
nadvolod <nadvolod@gmail.com>
2020-01-09 11:28:42 -0500

    update to appium v4

--- SauceExamples/Appium4.NUnit.Scripts/RealDevices/NativeApp/AndroidGetStarted.cs
+++ SauceExamples/Appium4.NUnit.Scripts/RealDevices/NativeApp/AndroidGetStarted.cs
@@ -3,6 +3,9 @@ using System.Reflection;
 using Common;
 using NUnit.Framework;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Appium.Enums;
 using OpenQA.Selenium.Remote;
 
 namespace Appium4.NUnit.Scripts.RealDevices.NativeApp
@@ -11,7 +14,7 @@ namespace Appium4.NUnit.Scripts.RealDevices.NativeApp
     public class AndroidGetStarted
     {
         private static string RdcUsHubUrl => "https://us1.appium.testobject.com/wd/hub";
-        private IWebDriver _driver;
+        private AndroidDriver<AndroidElement> _driver;
 
         [Test]
         [Category("Android")]
@@ -23,23 +26,23 @@ namespace Appium4.NUnit.Scripts.RealDevices.NativeApp
 
         public void ShouldOpenApp()
         {
-            var capabilities = new DesiredCapabilities();
+            var capabilities = new AppiumOptions();
             //We can run on any version of the platform as long as it's the correct device
             //Make sure to pick an Android or iOS device based on your app
-            capabilities.SetCapability("deviceName", "Google Pixel");
-            capabilities.SetCapability("platformName", "Android");
+            capabilities.AddAdditionalCapability(MobileCapabilityType.DeviceName, "Google Pixel");
+            capabilities.AddAdditionalCapability(MobileCapabilityType.PlatformName, "Android");
 
             /*
              * !!!!!!
              * TODO first you must upload an app to RDC so that you get your app key
              * Then, make sure you can hardcode it here just to get started
              */
-            capabilities.SetCapability("testobject_api_key", new ApiKeys().Rdc.Apps.SampleAppAndroid);
-            capabilities.SetCapability("name", MethodBase.GetCurrentMethod().Name);
-            capabilities.SetCapability("newCommandTimeout", 90);
+            capabilities.AddAdditionalCapability("testobject_api_key", new ApiKeys().Rdc.Apps.SampleAppAndroid);
+            capabilities.AddAdditionalCapability("name", TestContext.CurrentContext.Test.Name);
+            capabilities.AddAdditionalCapability("newCommandTimeout", 90);
 
-            _driver = new RemoteWebDriver(new Uri(RdcUsHubUrl), capabilities,
-                TimeSpan.FromSeconds(300));
+            //60 seconds for the connection timeout
+            _driver = new AndroidDriver<AndroidElement>(new Uri(RdcUsHubUrl), capabilities);
             var size = _driver.Manage().Window.Size;
             Assert.AreNotEqual(0, size.Height);
             
--- SauceExamples/Appium4.NUnit.Scripts/RealDevices/NativeApp/AndroidStatusUpdate.cs
+++ SauceExamples/Appium4.NUnit.Scripts/RealDevices/NativeApp/AndroidStatusUpdate.cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using System.Reflection;
 using Common;
 using NUnit.Framework;
 using NUnit.Framework.Interfaces;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Appium.Enums;
 using OpenQA.Selenium.Remote;
 using RestSharp;
 
@@ -18,29 +22,29 @@ namespace Appium4.NUnit.Scripts.RealDevices.NativeApp
          * Selenium.Nunit.Framework.BestPractices.test
          */
         private static string RdcUsHubUrl => "https://us1.appium.testobject.com/wd/hub";
-        private RemoteWebDriver _driver;
-        private DesiredCapabilities _capabilities;
+        private AndroidDriver<AndroidElement> _driver;
         private SessionId _sessionId;
 
         [SetUp]
         public void Setup()
         {
-            _capabilities = new DesiredCapabilities();
+            var capabilities = new AppiumOptions();
             //We can run on any version of the platform as long as it's the correct device
             //Make sure to pick an Android or iOS device based on your app
-            _capabilities.SetCapability("deviceName", "Google Pixel");
-            _capabilities.SetCapability("platformName", "Android");
+            capabilities.AddAdditionalCapability(MobileCapabilityType.DeviceName, "Google Pixel");
+            capabilities.AddAdditionalCapability(MobileCapabilityType.PlatformName, "Android");
 
             /*
              * !!!!!!
              * TODO first you must upload an app to RDC so that you get your app key
              * Then, make sure you can hardcode it here just to get started
              */
-            _capabilities.SetCapability("testobject_api_key", new ApiKeys().Rdc.Apps.SampleAppAndroid);
-            _capabilities.SetCapability("newCommandTimeout", 90);
+            capabilities.AddAdditionalCapability("testobject_api_key", new ApiKeys().Rdc.Apps.SampleAppAndroid);
+            capabilities.AddAdditionalCapability("name", TestContext.CurrentContext.Test.Name);
+            capabilities.AddAdditionalCapability("newCommandTimeout", 90);
 
-            _driver = new RemoteWebDriver(new Uri(RdcUsHubUrl), _capabilities,
-                TimeSpan.FromSeconds(300));
+            //60 seconds for the connection timeout
+            _driver = new AndroidDriver<AndroidElement>(new Uri(RdcUsHubUrl), capabilities);
         }
         [TearDown]
         public void Teardown()
