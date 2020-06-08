94501503c7591cf01aef435e04abbc09250f389f
nadvolod <nadvolod@gmail.com>
2020-06-08 15:31:39 -0400

    add sanity test

---
+++ SauceExamples/DotnetCore/Core.Selenium4.MsTest.Scripts/SanityTest.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Core.Selenium4.MsTest.Scripts
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
