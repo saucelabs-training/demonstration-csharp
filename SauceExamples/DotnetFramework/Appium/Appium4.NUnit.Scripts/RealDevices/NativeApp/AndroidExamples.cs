93c3ddf34699044e7e90232a05f029104799767f
nadvolod <nadvolod@gmail.com>
2020-07-15 15:23:46 -0400

    Updated Android RDC tests

--- SauceExamples/DotnetFramework/Appium/Appium4.NUnit.Scripts/RealDevices/NativeApp/AndroidExamples.cs
+++ SauceExamples/DotnetFramework/Appium/Appium4.NUnit.Scripts/RealDevices/NativeApp/AndroidExamples.cs
@@ -22,7 +22,7 @@ namespace Appium4.NUnit.Scripts.RealDevices.NativeApp
          * Selenium.Nunit.Framework.BestPractices.test
          */
         private static string RdcUsHubUrl => "https://us1.appium.testobject.com/wd/hub";
-        private AndroidDriver<AndroidElement> _driver;
+        private AppiumDriver<AndroidElement> _driver;
         private SessionId _sessionId;
 
         [SetUp]
@@ -31,14 +31,17 @@ namespace Appium4.NUnit.Scripts.RealDevices.NativeApp
             var capabilities = new AppiumOptions();
             //We can run on any version of the platform as long as it's the correct device
             //Make sure to pick an Android or iOS device based on your app
-            capabilities.AddAdditionalCapability(MobileCapabilityType.DeviceName, "Google Pixel");
+            capabilities.AddAdditionalCapability(MobileCapabilityType.DeviceName, "Google Pixel 4");
             capabilities.AddAdditionalCapability(MobileCapabilityType.PlatformName, "Android");
             //make sure you set locale as sometimes it opens in a different location and throws off locations
             capabilities.AddAdditionalCapability("locale", "en");
             capabilities.AddAdditionalCapability("language", "en");
             //The next major version of Appium (2.x) will **require** this capability
             capabilities.AddAdditionalCapability("automationName", "UiAutomator2");
-
+            //It's a good practice to set an appWaitActivity so that the automation knows when the app is loaded
+            capabilities.AddAdditionalCapability("appWaitActivity", "com.swaglabsmobileapp.MainActivity");
+            //It's a good practice to use the latest appium version
+            capabilities.AddAdditionalCapability("appiumVersion", "1.17.1");
             /*
              * !!!!!!
              * TODO first you must upload an app to RDC so that you get your app key
@@ -49,6 +52,13 @@ namespace Appium4.NUnit.Scripts.RealDevices.NativeApp
             //It's important to keep the newCommandTimeout on the higher end as Real Devices are slow
             capabilities.AddAdditionalCapability("newCommandTimeout", 180);
 
+            /*
+             Getting Error: OpenQA.Selenium.WebDriverException : The HTTP request to the remote WebDriver server for URL 
+            https://us1.appium.testobject.com/wd/hub/session timed out after 60 seconds.
+                ----> System.Net.WebException : The operation has timed out
+
+            Solution: Try changing to a more popular device
+             */
             _driver = new AndroidDriver<AndroidElement>(new Uri(RdcUsHubUrl), capabilities);
         }
         [TearDown]
@@ -72,9 +82,16 @@ namespace Appium4.NUnit.Scripts.RealDevices.NativeApp
 
         public void ShouldOpenApp()
         {
-            var size = _driver.Manage().Window.Size;
-            Assert.AreNotEqual(0, size.Height);
+            Assert.DoesNotThrow(() => LoginScreenIsVisible());
         }
+
+        private void LoginScreenIsVisible()
+        {
+            var wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(20));
+            wait.Until(ExpectedConditions.ElementIsVisible(
+                MobileBy.AccessibilityId("test-Username")));
+        }
+
         [Test]
         [Category("Android")]
         [Category("SimpleTest")]
