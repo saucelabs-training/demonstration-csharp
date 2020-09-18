ed2db973da686b3fe6fdf96cece497349cf755c7
nadvolod <nadvolod@gmail.com>
2020-09-18 13:33:30 -0400

    add ios example

---
+++ SauceExamples/DotnetFramework/Appium/Appium4.NUnit.Scripts/RealDevices/NativeApp/UP/AndroidGetStarted.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Common.SauceLabs;
+using NUnit.Framework;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Appium.Enums;
+
+namespace Appium4.NUnit.Scripts.RealDevices.NativeApp.UP
+{
+    [TestFixture]
+    [Parallelizable]
+
+    public class AndroidGetStarted
+    {
+        private static string HubUrl => "ondemand.us-west-1.saucelabs.com/wd/hub";
+        private AndroidDriver<AndroidElement> _driver;
+
+        [Test]
+        [Category("Android")]
+        [Category("SimpleTest")]
+        [Category("Rdc")]
+        [Category("NativeApp")]
+        [Category("Appium4NUnitScripts")]
+
+        public void ShouldOpenNativeAndroidApp()
+        {
+            var capabilities = new AppiumOptions();
+            //We can run on any version of the platform as long as it's the correct device
+            //Make sure to pick an Android or iOS device based on your app
+            capabilities.AddAdditionalCapability(MobileCapabilityType.DeviceName, "Google Pixel 4");
+            capabilities.AddAdditionalCapability(MobileCapabilityType.PlatformName, "Android");
+
+            /*
+             * !!!!!!
+             * TODO first you must upload an app to RDC so that you get your app key
+             * Then, make sure you can hardcode it here just to get started
+             */
+            capabilities.AddAdditionalCapability("testobject_api_key", new ApiKeys().Rdc.Apps.SampleAppAndroid);
+            capabilities.AddAdditionalCapability("name", TestContext.CurrentContext.Test.Name);
+            capabilities.AddAdditionalCapability("newCommandTimeout", 90);
+            //TODO it's a best practice to set the appium version so that you're always getting the latest
+            capabilities.AddAdditionalCapability("appiumVersion", "1.16.0");
+
+
+            //60 seconds for the connection timeout
+            _driver = new AndroidDriver<AndroidElement>(new Uri(HubUrl), capabilities);
+            var size = _driver.Manage().Window.Size;
+            Assert.AreNotEqual(0, size.Height);
+
+            //Always making sure to end the session at the end of any test
+            _driver?.Quit();
+        }
+    }
+}
---
+++ SauceExamples/DotnetFramework/Appium/Appium4.NUnit.Scripts/RealDevices/NativeApp/UP/GetStartedIos.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Common.SauceLabs;
+using NUnit.Framework;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Enums;
+using OpenQA.Selenium.Appium.iOS;
+
+namespace Appium4.NUnit.Scripts.RealDevices.NativeApp.UP
+{
+    [TestFixture]
+    [Parallelizable]
+    public class GetStartedIos
+    {
+        private static string HubUrl => "ondemand.us-west-1.saucelabs.com/wd/hub";
+        private IOSDriver<IOSElement> _driver;
+
+        [Test]
+        [Category("SimpleTest")]
+        [Category("NativeApp")]
+
+        public void ShouldOpenNativeIosApp()
+        {
+            var capabilities = new AppiumOptions();
+            //We can run on any version of the platform as long as it's the correct device
+            //Make sure to pick an Android or iOS device based on your app
+            capabilities.AddAdditionalCapability(MobileCapabilityType.DeviceName, "iPhone 11 Pro Max");
+            capabilities.AddAdditionalCapability(MobileCapabilityType.PlatformName, "iOS");
+            capabilities.AddAdditionalCapability(MobileCapabilityType.Language, "en");
+
+            capabilities.AddAdditionalCapability("name", TestContext.CurrentContext.Test.Name);
+
+            /*
+             * You need to upload your own Native Mobile App to Sauce Storage!
+             * https://wiki.saucelabs.com/display/DOCS/Uploading+your+Application+to+Sauce+Storage
+             *
+             * You can use either storage:<app-id> or storage:filename=
+             */
+            capabilities.AddAdditionalCapability("app", 
+                "storage:filename=iOS.RealDevice.SauceLabs.Mobile.Sample.app.2.3.0.ipa");
+            var sauceUser = Environment.GetEnvironmentVariable("SAUCE_USERNAME", EnvironmentVariableTarget.User);
+            var sauceAccessKey = Environment.GetEnvironmentVariable("SAUCE_ACCESS_KEY", EnvironmentVariableTarget.User);
+
+            var uri = $"https://{sauceUser}:{sauceAccessKey}@{HubUrl}";
+            //60 seconds for the connection timeout
+            _driver = new IOSDriver<IOSElement>(new Uri(uri), capabilities);
+            var size = int.Parse(_driver.Manage().Window.Size.Height.ToString());
+            Assert.Greater(size, 0);
+
+            //Always making sure to end the session at the end of any test
+            _driver?.Quit();
+        }
+    }
+}
