7680da4747f1dde6b5f82e50c51f68b851703993
nadvolod <nadvolod@gmail.com>
2020-12-14 10:57:23 -0500

    Capable of running in parallel at method level for iOS and Android

---
+++ SauceExamples/Core.Appium.MsTest.BestPractices/Tests/AndroidTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Appium.Enums;
+using OpenQA.Selenium.Appium.iOS;
+
+namespace Core.Appium.MsTest.BestPractices.Tests
+{
+    public class AndroidTest : BaseTest
+    {
+        public AndroidDriver<AndroidElement> Driver;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            var sauceUser = Environment.GetEnvironmentVariable("SAUCE_USERNAME", EnvironmentVariableTarget.User);
+            var sauceAccessKey = Environment.GetEnvironmentVariable("SAUCE_ACCESS_KEY", EnvironmentVariableTarget.User);
+            var uri = $"https://{sauceUser}:{sauceAccessKey}@{HubUrlPart}";
+
+            var capabilities = new AppiumOptions();
+            //We can run on any version of the platform as long as it's the correct device
+            //Make sure to pick an Android or iOS device based on your app
+            capabilities.AddAdditionalCapability(MobileCapabilityType.DeviceName, "Google Pixel 4");
+            capabilities.AddAdditionalCapability(MobileCapabilityType.PlatformName, "Android");
+            capabilities.AddAdditionalCapability("newCommandTimeout", 90);
+            capabilities.AddAdditionalCapability("name", TestContext.TestName);
+
+
+            /*
+             * You need to upload your own Native Mobile App to Sauce Storage!
+             * https://wiki.saucelabs.com/display/DOCS/Uploading+your+Application+to+Sauce+Storage
+             * You can use either storage:<app-id> or storage:filename=
+             */
+            capabilities.AddAdditionalCapability("app",
+                "storage:filename=Android.SauceLabs.Mobile.Sample.app.2.7.0.apk");
+
+
+            /*
+             Getting Error: OpenQA.Selenium.WebDriverException : The HTTP request to the remote WebDriver server for URL 
+            https://us1.appium.testobject.com/wd/hub/session timed out after 60 seconds.
+                ----> System.Net.WebException : The operation has timed out
+            Solution: Try changing to a more popular device
+             */
+            Driver = new AndroidDriver<AndroidElement>(new Uri(uri), capabilities);
+        }
+        [TestCleanup]
+        public void Teardown()
+        {
+            if (Driver == null) return;
+
+            var isTestPassed = TestContext.CurrentTestOutcome == UnitTestOutcome.Passed;
+            ((IJavaScriptExecutor)Driver).ExecuteScript("sauce:job-result=" + (isTestPassed ? "passed" : "failed"));
+            Driver.Quit();
+        }
+    }
+}
