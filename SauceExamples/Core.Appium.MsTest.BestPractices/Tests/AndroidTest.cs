fa76c998e8d6ec3120b86273f7f7d7f1f377728a
nadvolod <nadvolod@gmail.com>
2020-12-14 15:28:15 -0500

    Remove some dead comments

--- SauceExamples/Core.Appium.MsTest.BestPractices/Tests/AndroidTest.cs
+++ SauceExamples/Core.Appium.MsTest.BestPractices/Tests/AndroidTest.cs
@@ -9,8 +9,14 @@ using TestContext = NUnit.Framework.TestContext;
 
 namespace Core.Appium.Nunit.BestPractices.Tests
 {
-    public class AndroidTest : BaseTest
+    public class AndroidTest
     {
+        public static string HubUrlPart => "ondemand.us-west-1.saucelabs.com/wd/hub";
+        public string SauceUser => Environment.GetEnvironmentVariable("SAUCE_USERNAME", EnvironmentVariableTarget.User);
+        public string SauceAccessKey => Environment.GetEnvironmentVariable("SAUCE_ACCESS_KEY", EnvironmentVariableTarget.User);
+
+        public string Url => $"https://{SauceUser}:{SauceAccessKey}@{HubUrlPart}";
+
         public AndroidDriver<AndroidElement> Driver { get; set; }
 
         [TestInitialize]
--- SauceExamples/Web.Tests/BestPractices/test/BaseTest.cs
+++ SauceExamples/Web.Tests/BestPractices/test/BaseTest.cs
@@ -11,20 +11,8 @@ using RestSharp.Authenticators;
 
 namespace Selenium3.Nunit.Framework.BestPractices.test
 {
-    //TODO this whole class is a duplication of BaseCrossBrowserTEst.cs
-    //The reason why it was duplicated was because I needed to be able to configure
-    //the [Setup] methods. Some needed to be able to set the Build Name and do some actions,
-    //other tests didn't need to set the build name, and others, only needed to set
-    //the build name. It seems as though maybe a Strategy pattern might solve these problems
-    //It might make sense to create a ISetupStrategy that is defined in the constructor
-    //of every single feature file. That feature file will define the setup Strategy.
-    //Then, those operations will be performed int the [Setup] of the BaseTest
     [TestFixture]
-    [Category("AcceptanceTests")]
-    [Category("CrossBrowser")]
-    [Category("NUnit")]
     [Category("BestPractices")]
-    [Category("NunitFramework")]
 
     public class BaseTest
     {
