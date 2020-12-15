492eaa2c82aa553cf7b548ec3808bcf22e0b3b96
nadvolod <nadvolod@gmail.com>
2020-12-15 14:14:23 -0500

    Add page objects for iOS tests

--- SauceExamples/Core.Appium.MsTest.BestPractices/Tests/IOSFeatures.cs
+++ SauceExamples/Core.Appium.MsTest.BestPractices/Tests/IOSFeatures.cs
@@ -1,61 +1,32 @@
-﻿using System;
-using Common.TestData;
+﻿using Common.TestData;
 using FluentAssertions;
+using Core.Appium.Nunit.BestPractices.Screens.iOS;
 using NUnit.Framework;
-using OpenQA.Selenium;
-using OpenQA.Selenium.Appium;
-using OpenQA.Selenium.Support.UI;
-using static SeleniumExtras.WaitHelpers.ExpectedConditions;
 
 namespace Core.Appium.Nunit.BestPractices.Tests
 {
     [TestFixtureSource(typeof(DeviceCombinations), nameof(DeviceCombinations.PopularIosDevices))]
     [Parallelizable]
-    public class IOSFeatures : IosTest
+    public class IosFeatures : IosTest
     {
-        public IOSFeatures(string deviceName) : base(deviceName)
+        public IosFeatures(string deviceName) : base(deviceName)
         {
         }
 
         [Test]
         public void ShouldOpenApp()
         {
-            Action screenIsVisible = LoginScreenIsVisible;
-            screenIsVisible.Should().NotThrow();
-        }
-
-        private void LoginScreenIsVisible()
-        {
-            var wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(20));
-            wait.Until(ElementIsVisible(
-                MobileBy.AccessibilityId("test-Username")));
+            var loginScreen = new LoginScreen(Driver);
+            loginScreen.IsVisible().Should().NotThrow();
         }
 
         [Test]
         public void ShouldLogin()
         {
-            var wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(20));
+            var loginScreen = new LoginScreen(Driver);
+            loginScreen.Login("standard_user", "secret_sauce");
 
-            var userName = wait.Until(ElementIsVisible(
-                MobileBy.AccessibilityId("test-Username")));
-            userName.SendKeys("standard_user");
-
-            var password = wait.Until(ElementIsVisible(
-                MobileBy.AccessibilityId("test-Password")));
-            password.SendKeys("secret_sauce");
-
-            var login = wait.Until(ElementIsVisible(
-                MobileBy.AccessibilityId("test-LOGIN")));
-            login.Click();
-
-            Action isCartVisible = () => GetCartElement(wait);
-            isCartVisible.Should().NotThrow();
-        }
-
-        private void GetCartElement(WebDriverWait wait)
-        {
-            wait.Until(ElementIsVisible(
-                By.Name("test-Cart")));
+            new ProductsScreen(Driver).IsVisible().Should().NotThrow();
         }
     }
 }
