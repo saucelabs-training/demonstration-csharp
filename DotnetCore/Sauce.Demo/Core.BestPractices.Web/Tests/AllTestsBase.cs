fbcc568b511f4746c2c2d5f8c3c5fefc983ba974
nikolay-a <nadvolod@gmail.com>
2021-06-08 14:25:18 -0400

    Cross-platform visual e2e testing enabled

--- DotnetCore/Sauce.Demo/Core.BestPractices.Web/Tests/AllTestsBase.cs
+++ DotnetCore/Sauce.Demo/Core.BestPractices.Web/Tests/AllTestsBase.cs
@@ -14,20 +14,5 @@ namespace Core.BestPractices.Web.Tests
         public string SauceAccessKey;
         public Dictionary<string, object> SauceOptions;
         public string ScreenerApiKey;
-
-        [SetUp]
-        public void Setup()
-        {
-            SauceUserName = Environment.GetEnvironmentVariable("SAUCE_USERNAME", EnvironmentVariableTarget.User);
-            SauceAccessKey = Environment.GetEnvironmentVariable("SAUCE_ACCESS_KEY", EnvironmentVariableTarget.User);
-            ScreenerApiKey = Environment.GetEnvironmentVariable("SCREENER_API_KEY", EnvironmentVariableTarget.User);
-            SauceOptions = new Dictionary<string, object>
-            {
-                ["username"] = SauceUserName,
-                ["accessKey"] = SauceAccessKey,
-                ["name"] = TestContext.CurrentContext.Test.Name
-            };
-        }
-
     }
 }
